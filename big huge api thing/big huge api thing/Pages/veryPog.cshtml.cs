9ae8fd73ca27f1e6cd1c7e8e51acbbc26a2675d3
logan-webber-IW <logan.webber@integration.works>
2021-10-26 15:21:27 +1300

    added new page

---
+++ big huge api thing/big huge api thing/Pages/veryPog.cshtml.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace big_huge_api_thing.Pages
+{
+    public class VeryPogModel : PageModel
+    {
+        public void OnGet()
+        {
+            Console.WriteLine("Skate fast eat ass die fast");
+        }
+    }
+}
